e95b28ec26cacbd12f18514d9982ce37f6807398
nei <nei.thomass@gmail.com>
2021-08-02 11:39:41 -0300

    Enviando inicio do projeto
    Desenvolvendo primeiros arquivos iniciados com vue-cli e alguns pacotes instalados manualmente

---
+++ README.md
@@ -0,0 +1,3 @@
+# Travel landing - web single page
+
+Single page web application
---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container-fluid m-0 p-0">
+        <h1>Home Page</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
