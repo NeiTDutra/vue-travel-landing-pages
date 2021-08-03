356a376fdbd7331f04ba77285debce4120b49918
nei <nei.thomass@gmail.com>
2021-08-03 09:34:13 -0300

    adicionando autenticação por email

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -1,11 +1,12 @@
 
-const firebaseConfig = {
+export default {
+  firebaseConfig: {
     apiKey: "AIzaSyD4q-Kh1jtbSGOzwjVls_l7tPK1_RmruUI",
     authDomain: "travel-landing-4d56b.firebaseapp.com",
     projectId: "travel-landing-4d56b",
     storageBucket: "travel-landing-4d56b.appspot.com",
     messagingSenderId: "922960472570",
     appId: "1:922960472570:web:99475f5dc5ca5cdd1adc43"
-  };
-
-export default firebaseConfig;
+  }
+  
+} 
